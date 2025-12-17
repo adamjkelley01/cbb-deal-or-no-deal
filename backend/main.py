f44d8a86d52a3e4f1dc63ef367cbb6c5dfe5d7fd
adamjkelley01 <adamjkelley01@gmail.com>
2025-12-17 01:43:34 -0700

    updated connections

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://cbb-deal-or-no-deal-jzhr.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
